4b74ab12b1491a5b4a80d6807d752f3c7be5e18d
Agostino <agostinoarcadi@gmail.com>
2023-11-22 13:00:54 +0100

    Modificato classi Forma e Rettangolo

--- src/Forma.java
+++ src/Forma.java
@@ -1,4 +1,29 @@
 public class Forma {
+    private int base;
+
+    private int altezza;
+
+    public Forma(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    public int getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(int altezza) {
+        this.altezza = altezza;
+    }
+
     public void calcolaArea() {
         System.out.println("calcolo area");
     }
--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -1,32 +1,11 @@
 public class Rettangolo extends Forma {
-    private int base;
-
-    private int altezza;
 
     public Rettangolo(int base, int altezza) {
-        this.base = base;
-        this.altezza = altezza;
-    }
-
-    public int getBase() {
-        return base;
-    }
-
-    public void setBase(int base) {
-        this.base = base;
-    }
-
-    public int getAltezza() {
-        return altezza;
-    }
-
-    public void setAltezza(int altezza) {
-        this.altezza = altezza;
+        super(base, altezza);
     }
 
     @Override
     public void calcolaArea() {
-
-        System.out.println(base * altezza);
+        System.out.println(getBase() * getAltezza());
     }
 }
